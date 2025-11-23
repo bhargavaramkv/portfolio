e7f26fb1141bab393d20a3c56e201538f75c4117
Bhargava <kattavenkatabhargavaram@gmail.com>
2025-11-23 19:57:59 +0530

    Fix GitHub Pages deployment - update terminal hero and fix build paths

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { ArrowDown, FileText, Send, Terminal, Code2, Cpu } from 'lucide-react';
 
 export const Hero: React.FC = () => {
